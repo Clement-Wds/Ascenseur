04593241b235bd98e7ff4a7a44f15e713d01f84b
Clement-Wds <clementwalshdeserrant@mail.com>
2021-05-05 16:08:33 +0200

    main

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-print('hello world')
+def main():
+    print('---ASCENSEUR---')
+    print('1 : MONTER')
+    print('2 : DESCENDRE')
+    choice = input()
+    if choice == 1:
+        goUp()
+    elif choice == 2:
+        goDown()
+    else: 
+        print('Veuillez entrez une valeur valide')
+
+
 
 def goUp():
-    #goUp
+    print('l\'ascenseur monte')
 
 def goDown():
-    #goDown
+    print('l\'ascenseur descend')
+
+main()
+goUp()
